e4a7c3a4c6c116dd29e5bc563ab862282ca74758
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2023-03-26 11:39:09 +0300

    Create flag.sh

---
+++ Linux/Network cat (100)/flag.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Nothing to see here... sorry"
+sleep 1
+echo "CTRL+C to exit the program"
+sleep 5
+echo "You are learning. Good."
+echo "There is your flag: flag{nc_adventures}"
