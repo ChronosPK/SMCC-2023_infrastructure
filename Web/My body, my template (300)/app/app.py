34d1e773d10e58497179206117d1f19a9bf7c817
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2023-03-26 12:03:05 +0300

    Create app.py

---
+++ Web/My body, my template (300)/app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    if request.args.get('c'):
+        return render_template_string(request.args.get('c'))
+    else:
+        return "Hai sa facem si noi ceva, zic si eu..."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8005, debug=True, threaded=True, use_evalex=False)
